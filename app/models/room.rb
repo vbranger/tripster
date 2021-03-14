0319ab428d599658c28b456eb3ccb99cca88d4eb
Victor Branger <victor.branger@gmail.com>
2021-03-14 12:07:41 +0100

    photo working

--- app/models/room.rb
+++ app/models/room.rb
@@ -10,6 +10,12 @@ class Room < ApplicationRecord
     return self.web_id = match_data[:id]
   end
 
+  def get_img_url(value)
+    match_data = value.match(/(https:.+")/)
+    p self.photo = match_data[0][0..-2]
+    return self.photo
+  end
+
   def scrap
     url = "https://www.airbnb.fr/embeddable/home?id=#{self.web_id}"
     # url = "https://medium.com/@LindaVivah/the-beginner-s-guide-scraping-in-ruby-cheat-sheet-c4f9c26d1b8c"
@@ -41,6 +47,10 @@ class Room < ApplicationRecord
     html_doc = Nokogiri::HTML(br.body)
     p house_name = html_doc.search("#listing-#{self.web_id} > div._hgs47m > div > div > a > div._wlyu2v > span > span").children.text
     self.name = house_name
+    p photo = html_doc.search("#listing-#{self.web_id} > div._1dp4576 > div._e296pg > div._gjw2an > div > div > div")
+    unless photo.empty?
+      p get_img_url(photo.last.attributes["style"].value)
+    end
     br.quit
   end
 

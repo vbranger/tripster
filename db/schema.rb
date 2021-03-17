ee98cb53ca42ff7a0d41449fbc69968af8b8e1e8
Victor Branger <victor.branger@gmail.com>
2021-03-17 12:03:48 +0100

    reboot acts as votable

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_14_192855) do
+ActiveRecord::Schema.define(version: 2021_03_17_110320) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"

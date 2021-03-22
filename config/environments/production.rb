62a14687a5881a8b62d427ae1a3eafe99e442a65
Victor Branger <victor.branger@gmail.com>
2021-03-22 15:27:11 +0100

    mailjet setup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,11 +2,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   config.action_mailer.default_url_options = { host: ENV['URL'] }
-  config.action_mailer.delivery_method = :sendgrid_actionmailer
-  config.action_mailer.sendgrid_actionmailer_settings = {
-    api_key: ENV['SENDGRID_API_KEY'],
-    raise_delivery_errors: true
-  }
+  config.action_mailer.delivery_method = :mailjet
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.

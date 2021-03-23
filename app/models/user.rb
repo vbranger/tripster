fe43c39ed8e7a6218e301f8bb9920d44500e7c73
Victor Branger <victor.branger@gmail.com>
2021-03-23 20:03:25 +0100

    added confirmable to devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,16 @@
 class User < ApplicationRecord
-  after_create :send_welcome_email
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :confirmable
   has_many :participants, dependent: :destroy
   has_many :trips, through: :participants
   validates :first_name, presence: true
   validates :last_name, presence: true
   acts_as_voter
 
-  private
-
-  def send_welcome_email
+  # Override Devise::Confirmable#after_confirmation
+  def after_confirmation
     UserMailer.welcome(self).deliver_now
   end
   

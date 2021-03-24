962328ff9f5fcc7eb7f4be64e3c3ae0b2860afbb
Victor Branger <victor.branger@gmail.com>
2021-03-24 15:22:31 +0100

    added automatic signin after confirmation email

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => { :registrations => "users/registrations"}
+  devise_for :users, :controllers => { :registrations => "users/registrations",
+                                       :confirmations => "users/confirmations" }
   root to: 'trips#index'
 
   get 'waiting_confirmation', to: 'pages#waiting_confirmation'

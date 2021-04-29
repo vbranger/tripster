1bc32f23ba06aa1fd3e3acef42092b42e78d2ec7
Victor Branger <victor.branger@gmail.com>
2021-04-29 15:11:03 +0200

    refacto

--- app/models/news.rb
+++ app/models/news.rb
@@ -4,29 +4,33 @@ class News < ApplicationRecord
   
   def time_since_created
     # secondes since creation
-    time_ellapsed = Time.now - self.created_at
+    time_ellapsed = (Time.now - self.created_at).to_i
+    p time_ellapsed
 
     if time_ellapsed < 60 # Moins d'une minute
-      "Il y a #{time_ellapsed.to_i} secondes"
+      "Il y a #{time_ellapsed} #{time_ellapsed ? "seconde" : "secondes"}"
     elsif time_ellapsed < 3600 # Mois d'une heure
-      "Il y a #{time_ellapsed.to_i / 60} minutes"
-    elsif time_ellapsed < 86400 # Moins d'une journée
-      "Il y a #{time_ellapsed.to_i / 3600 } heures"
+      "Il y a #{time = time_ellapsed / 60} #{time <= 1 ? "minute" : "minutes"}"
+    elsif time_ellapsed < 86_400 # Moins d'une journée
+      "Il y a #{time_ellapsed / 3_600} #{time <= 1 ? "heure" : "heures"}"
     else
-      "Il y a #{time_ellapsed.to_i / 86400 } jours"
+      "Il y a #{time_ellapsed / 86_400} #{time <= 1 ? "jour" : "jours"}"
     end
   end
 
   def write_content
+
+    user_link = "<a href='/user/#{self.user.id}'>#{self.user.first_name}</a>"
+    trip_link = "<a href='/trips/#{self.trip_id}'>#{Trip.find(self.trip_id).name}</a>"
     case self.action_type 
     when "trips#update"
-      self.update(content: "<a href='/user/#{self.user.id}'>#{self.user.first_name}</a> a défini une nouvelle destination : #{Trip.find(self.trip_id).destination}")
+      self.update(content: "#{user_link} a défini une nouvelle destination : #{Trip.find(self.trip_id).destination}")
     when "trips#create"
-      self.update(content: "#{self.user.first_name} a créé <a href='/trips/#{self.trip_id}'>#{Trip.find(self.trip_id).name}</a>")
+      self.update(content: "#{user_link} a créé #{trip_link}")
     when "invites#create"
-      self.update(content: "#{self.user.first_name} a rejoint #{Trip.find(self.trip_id).name}")
+      self.update(content: "#{user_link} a rejoint le trip")
     when "participants#destroy"
-      self.update(content: "#{self.user.first_name} a quitté #{Trip.find(self.trip_id).name}")
+      self.update(content: "#{user_link} a quitté #{trip_link}")
     else
       self.update(content: "Autre info")
     end

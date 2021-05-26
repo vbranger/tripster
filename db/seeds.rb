8072be39b4500fa21878e55505f8ef4f81d83fc8
Florian Leroy <florian.daniel.leroy@gmail.com>
2021-05-26 11:59:38 +0200

    adding a tiny seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "Deleting users.."
+User.destroy_all
 puts "Creating users..."
-victor = User.create(email: 'victor@gmail.com', password: '123456')
-chaoui = User.create(email: 'chaoui@gmail.com', password: '123456')
+victor = User.create(email: 'victor@gmail.com', password: '123456', first_name: 'Victor', last_name: 'Branger')
+flo = User.create(email: 'florian@gmail.com', password: '123456', first_name: 'Florian', last_name: 'Leroy')
+puts "Seed finished..."

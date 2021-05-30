2430a8995e1fde10949a70762695f253a8bee196
Florian Leroy <florian.daniel.leroy@gmail.com>
2021-05-30 18:38:47 +0200

    improving validates

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -2,7 +2,7 @@ class Participant < ApplicationRecord
   belongs_to :user
   belongs_to :trip
   has_many :notifications, as: :imageable
-  validates :user_id, uniqueness: { scope: :trip_id }
+  validates :user_id, uniqueness: { scope: :trip_id, message: "already on the trip" }
 
   def active?(params, user, current_user)
     if params[:user_id].nil?

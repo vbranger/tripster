87047b1b602296a8575b38dcd8c4a9b417bfdd93
Victor Branger <victor.branger@gmail.com>
2021-06-14 15:32:00 +0200

    review test

--- spec/factories/factories.rb
+++ spec/factories/factories.rb
@@ -14,7 +14,22 @@ FactoryBot.define do
 
   factory :trip do
     name { 'test' }
-    user { User.new }
+    user { create(:user) }
+  end
+
+  factory :room do
+    trip { create(:trip) }
+    name { "test" }
+    price { rand(50.0..500.0).round(2) }
+    url { "a" }
+    avg_score { 0.0 }
+  end
+
+  factory :review do
+    content { "blabla" }
+    room { create(:room) }
+    user { create(:user) }
+    score { rand(0..5)}
   end
   
 end
---
+++ spec/models/review_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+RSpec.describe Review, type: :model do
+  subject {
+    described_class.new(
+      room: create(:room),
+      user: create(:user),
+      score: 5
+    )
+  }
+
+  it { should have_many(:news).dependent(:destroy) }
+  it { should validate_presence_of(:score) }
+  it do
+    should validate_inclusion_of(:score)
+      .in_array(%w[0 1 2 3 4 5])
+  end
+  it { should belong_to(:room) }
+  it { should belong_to(:user) }
+
+  describe "#update_avg_score" do
+    it "updates its room avg_score when a new Review is created" do
+      initial_score = subject.room.avg_score
+      create(:review, score: 5, room: subject.room)
+      subject.room.avg_score
+      expect(subject.room.avg_score).not_to eq(initial_score.to_i)
+    end
+
+    it "returns the average score of room reviews" do
+      room = create(:room)
+      
+      create(:review, score: 2, room: room, user: create(:user))
+      create(:review, score: 3, room: room, user: create(:user))
+      create(:review, score: 2, room: room, user: create(:user))
+      create(:review, score: 4, room: room, user: create(:user))
+      create(:review, score: 1, room: room, user: create(:user))
+
+      expect(room.avg_score).to eq(2.4)
+    end
+
+  end
+end

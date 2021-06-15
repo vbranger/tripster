2b21e7e59eb9203e11fbfc64bd760a6fc60eac40
Victor Branger <victor.branger@gmail.com>
2021-06-15 18:54:24 +0200

    airbnb scrap adapt if no dates provided

--- app/services/airbnb_scrap_service.rb
+++ app/services/airbnb_scrap_service.rb
@@ -1,53 +1,58 @@
 class AirbnbScrapService
-    def initialize(url)
-        @url = url
+  def initialize(url)
+    @url = url
+  end
+
+  def call
+    p exception_keys = [] << :price if (!@url.include?("check_in") || !@url.include?("check_out"))
+
+    browser = Ferrum::Browser.new({ timeout: 60, headless: true, process_timeout: 60 })
+    browser.go_to(@url)
+
+    attributes.except(*exception_keys).each_value {|attribute| wait_for(attribute[:css], browser)}
+    
+    html_doc = Nokogiri::HTML(browser.body)
+    result = {} 
+    attributes.except(*exception_keys).each do |k,attribute|
+      result[k] = attributes[k][:method].call(html_doc.search(attribute[:css]))
     end
+    browser.quit
+    result
+  end 
 
-    def call
-        br = Ferrum::Browser.new({ timeout: 60, headless: true, process_timeout: 60 })
-        br.go_to(@url)
-        attributes.values.each {|attribute| wait_for(attribute[:css], br)}
-        
-        html_doc = Nokogiri::HTML(br.body)
-        result = {} 
-        attributes.each do |k,attribute|
-            result[k] = attributes[k][:method].call(html_doc.search(attribute[:css]))
-        end
-        br.quit
-        result
-    end 
+  def attributes
+    {
+      name: {
+        css:"._9cqu50 > h1",
+        method: lambda {|var| var.text}
+      },
+      photo: {
+        css:"._6tbg2q",
+        method: lambda {|var| var.first.attributes["data-original-uri"].value}
+      },
+      price: {
+        css:"._hdznyn > ._1k4xcdh",
+        method: lambda {|var| var.children.text.gsub(/\D/, "").to_i}
+      }
+    }
+  end
 
-    def attributes
-        {
-          name: {
-              css:"._9cqu50 > h1",
-              method: lambda {|var| var.text}
-          },
-          photo: {
-            css:"._6tbg2q",
-            method: lambda {|var| var.first.attributes["data-original-uri"].value}
-          },
-          price: {
-            css:"._hdznyn > ._1k4xcdh",
-            method: lambda {|var| var.children.text.gsub(/\D/, "").to_i}
-          }
-        }
+
+  private 
+
+  def wait_for(css, browser)
+    node = nil
+    while node.nil?
+      p "waiting for #{css}"
+      node = browser.at_css(css)
+      sleep 0.1
     end
+    p "founded"
+    node
+  end
 
-
-    private 
-
-    def wait_for(css, br)
-        node = nil
-        while node.nil?
-            node = br.at_css(css)
-            sleep 0.1
-        end
-        node
-    end
-
-    def get_img_url(value)
-        match_data = value.match(/(https:.+")/)
-        match_data[0][0..-2]
-    end
+  def get_img_url(value)
+    match_data = value.match(/(https:.+")/)
+    match_data[0][0..-2]
+  end
 end

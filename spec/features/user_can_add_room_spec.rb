147da79b0804722ff96e8d96fb85d839ae334964
Victor Branger <victor.branger@gmail.com>
2021-06-28 11:32:21 +0200

    can cancel room creation after scrap

--- spec/features/user_can_add_room_spec.rb
+++ spec/features/user_can_add_room_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-feature "User can add a room index" do
+xfeature "User can add a room index" do
   context "when aasm(room_state) => propositions" do
     scenario "successfully" do
       user = sign_in
@@ -14,6 +14,24 @@ feature "User can add a room index" do
       find('.btn-primary').click
       find('.btn-primary').click
       expect(page).to have_css 'a', text: 'Ecrire un avis'
+    end
+  end
+end
+
+feature "User can cancel room creation" do
+  context "from edit page after scrap" do
+    scenario "successfully" do 
+      sign_in
+      trip = create(:trip, aasm_state: "propositions")
+      room = create(:room, name: "Maison en bord de mer", trip: trip)
+      p room
+      p trip.rooms
+      visit new_trip_room_path(trip)
+      fill_in "room_url", with: "https://www.booking.com/hotel/it/residence-150.fr.html"
+      find('.btn-primary').click
+      click_on('back-btn')
+      expect(page).not_to have_content('Ferrini')
+      expect(page).to have_content('Maison en bord de mer')
     end
   end
 end
